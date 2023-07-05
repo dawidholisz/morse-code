737f60e90786793197435d1a36d81afd4ebf65db
Dawid Holisz <dawid.holisz@gmail.com>
2023-07-05 21:26:57 +0200

    task implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useCallback, useState} from "react";
+import {useDebounce, useKeyPressEvent} from "react-use";
+import {morseCodeMap} from "./utils";
+
+const DASH_SIGN_DURATION = 1000
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [message, setMessage,] = useState<string>("");
+    const [sequence, setSequence,] = useState<string>("");
+    const [pressDownTimeStamp, setPressDownTimeStamp] = useState<number>(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [, cancelDecoding] = useDebounce(
+        () => {
+            const decodedCharacter = morseCodeMap.get(sequence)
+            if (decodedCharacter !== undefined)
+                setMessage(currentMessage => currentMessage.concat(decodedCharacter))
+            setSequence('')
+        },
+        DASH_SIGN_DURATION,
+        [sequence]
+    );
+
+    const handlePressDown = useCallback(() => {
+        cancelDecoding()
+        setPressDownTimeStamp(new Date().valueOf())
+    }, [])
+
+    const handlePressUp = useCallback(() => {
+        const pressDuration = new Date().valueOf() - pressDownTimeStamp
+        const character = pressDuration >= DASH_SIGN_DURATION ? "-" : "."
+        setSequence(currentSequence => currentSequence.concat(character))
+        setPressDownTimeStamp(0)
+    }, [pressDownTimeStamp])
+
+
+    useKeyPressEvent(' ', handlePressDown, handlePressUp)
+
+    return (
+        <>
+            <h1>Morse code</h1>
+
+            <h2>Decoded message: {message}</h2>
+            <h2>Current sequence: {sequence}</h2>
+            <button
+                type="button"
+                className="morse-btn"
+                onMouseDown={handlePressDown}
+                onMouseUp={handlePressUp}
+            >
+                Press and hold to encode Morse Code
+            </button>
+            <p className="read-the-docs">
+                yellow button - dot, green - dash,
+                dash - {DASH_SIGN_DURATION / 1000}s
+            </p>
+            <p className="read-the-docs">you can also use spacebar</p>
+        </>
+    )
 }
 
 export default App
